3024670c8ec608ef5b3bd924431abb50e2dc4787
VeronicaCastroMurillo <119750643+VeronicaCastroMurillo@users.noreply.github.com>
2023-07-20 05:46:57 +0000

    guardando progreso

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import '../../styles/home.css';
 import ReactDOM from "react-dom";
 import { useState } from "react";
@@ -10,7 +10,35 @@ import Task from "./Task";
 const Home = () => {
 	const [tasks, setTasks] = useState([])
 	const [InputText, setInputText] = useState("");
+	const todosApi = 'https://fake-todo-list-52f9a4ed80ce.herokuapp.com/todos/user/VeronicaCastroMurillo'
 
+	//GET API Todos
+	useEffect(() => {
+		//fetch(urlAPI).then(promesa).then(resultado).catch(mensaje de error)
+		fetch(todosApi).then(result => result.json()).then(data => {
+			console.log(data)
+			setTasks(data)
+		}
+		).catch(err => err)
+	}, []);
+
+	//PUT TO UPDATE DATA WHEN WE PRESS ENTER.
+	let newObj = [{"label": 'Hola', "done": false}]
+	
+	let updateAllTasks = () => {
+		useEffect(() => {
+			
+			fetch(todosApi, {
+				method: 'PUT',
+				body: JSON.stringify(newObj),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}).then(response=>response.json()).then(data => setTasks(newObj)).catch(err => err);
+		}, []);
+	}
+
+	//
 
 	const handleKeyDown = (e) => {
 		if (e.key === "Enter") {
@@ -29,6 +57,7 @@ const Home = () => {
 
 	}
 
+
 	return (
 		<div className="Input-center main-container">
 			<h1 className="title mt-5">Todos</h1>
@@ -39,12 +68,12 @@ const Home = () => {
 				{/* Render every input that gets inserted */}
 				{tasks.map((task, index) => {
 					return <li key={index} className="ul-list list-group-item">
-						<Task task={task} deleteTag={deleteTag} id={index} />
+						<Task task={task.label} deleteTag={deleteTag} id={index} />
 					</li>
 				})}
 				<li className="list-group-item Input-start">{tasks.length} Items left</li>
+				<button onClick={() => { alert('It´s working') }}>DELETE ALL</button>
 			</ul>
-
 
 		</div>
 	);
